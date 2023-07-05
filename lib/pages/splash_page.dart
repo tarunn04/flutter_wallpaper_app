efd7f16f0381118c79122f867322f4848ef9f9e6
tarunn04 <b.tarun10@gmail.com>
2023-07-05 21:11:28 +0530

    home page UI

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -2,34 +2,84 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:wallpaper_app/widgets/searchbar.dart';
 
-class SplashPage extends StatelessWidget {
-  const SplashPage({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    final double screenWidth = screenSize.width;
+    final double screenHeight = screenSize.height;
+
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-
-            const SizedBox(height: 50),
-
-            Text("HOME", style: TextStyle(
-              fontFamily: GoogleFonts.poppins().fontFamily,
-              fontWeight: FontWeight.bold,
-              fontSize: 35,
-            )),
-
-            const SizedBox(height: 10,),
-
-            const SearchBarWidget(),
-
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 50),
+              Text(
+                "HOME",
+                style: TextStyle(
+                  fontFamily: GoogleFonts.poppins().fontFamily,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const SearchBarWidget(),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: screenHeight * 0.3,
+                child: ListView.builder(
+                  itemCount: 20,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                      ),
+                      width: screenWidth * 0.5,
+                      margin: const EdgeInsets.symmetric(vertical: 3,horizontal: 4)
+                    );
+                  },
+                ),
+              ),
+              Text(
+                "Categories",
+                style: TextStyle(
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                    color:const Color.fromARGB(255, 45, 45, 45),
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold),
+              ),
+              GridView.builder(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 15,
+                  mainAxisSpacing: 15,
+                 childAspectRatio: 1.8,
+                ),
+                itemCount: 10, // Replace with your desired item count
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
